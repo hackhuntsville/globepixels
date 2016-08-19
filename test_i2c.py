120858915a5595e5d4128933d9b82d1dbabd0a54
Tyler Crumpton <tyler.crumpton@gmail.com>
2016-08-19 11:48:56 -0500

    Turn test into real bridge.

--- test_i2c.py
+++ test_i2c.py
@@ -1,11 +1,30 @@
-#from pyduinoincludes import *
+from pyduinoincludes import *
 
-SCL = 27 
-SDA = 29
+SCL = D5  #Uno A5 --> Pyduino D5
+SDA = D4  #Uno A4 --> Pyduino D4
 
-def send(addr, data):
+I2C_ADDR = "\x10"
+
+@setHook(HOOK_STARTUP)
+def init():
     i2cInit(True, SCL, SDA)
-    addr = "\x10"
-    i2cWrite(addr+data, 10, True)
-    return addr+data
+
+def report(color):
+
+    command = "!"
+    if color == "purple":
+        command = "1"
+    elif color == "red":
+        command = "2"
+    elif color == "green":
+        command = "3"
+    elif color == "blue":
+        command = "4"
+    elif color == "yellow":
+        command = "5"
+    elif color == "orange":
+        command = "6"
+
+    i2cWrite(I2C_ADDR + command, 10, True)
+    return command
 
